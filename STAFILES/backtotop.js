235dc46dc48b5bf77bacbdf18cfd5a2c901bba00
Acertv12 <kerwinr99@gmail.com>
2025-02-28 21:10:57 +0800

    Add files via upload

---
+++ STAFILES/backtotop.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+    let button = document.getElementById("backtotop");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
